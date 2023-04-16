98d53169e46f70226bf1744489e8bf6b6212f567
Guy Foster <125807123+guyfoz77@users.noreply.github.com>
2023-04-16 14:18:26 +0100

    Make canvas fill with pixels

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+let xAcross = 10;
 
+const canvas = document.querySelector('.canvas');
+
+function createPixel() {
+    let pixel = document.createElement('div');
+    pixel.classList.add('pixel');
+    return pixel;
+}
+
+function createRow(xAcross) {
+    let row = document.createElement('div')
+    row.classList.add('row');
+    for (let i = xAcross; i >= 1; i--) {
+        row.appendChild(createPixel());
+    }
+    return row;
+}
+
+function initialise(xAcross) {  
+    canvas.textContent = '';     
+    for (let i = xAcross; i >= 1; i--) {    
+        canvas.appendChild(createRow(xAcross));  //adds rows to the main container.
+    }
+//     const pixels = document.querySelectorAll('.pixel');
+
+//     pixels.forEach(pixel => {       //adds event listeners to all the pixels to change opacity on mouseover.
+//         pixel.style.opacity = 1;
+//         pixel.addEventListener('mouseover', function () {
+//             if (!rainbowMode) {
+//                 this.style.opacity -= opacityChange;
+//             } else {
+//                 this.style.backgroundColor = getRandomColour();
+//             }
+//         })
+// })
+}
+
+initialise(xAcross);
